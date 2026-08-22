549bfdfbb219b615fd03e210e810547a1a35d5bf
rewanayman2362005 <rewanayman2362005@gmail.com>
2026-08-22 20:14:16 +0300

    Quession2

--- Generic sheet route/Program.cs
+++ Generic sheet route/Program.cs
@@ -2,12 +2,38 @@
 namespace Generic_sheet_route
 {
     internal class Program
-    {
+    {       
+        #region Q2
+        //public class continer<T>
+        //{
+        //    public T item;
+
+        //    public void Add(T item)
+
+        //    { this.item=item
+        //          ; }
+
+        //    public T GET()
+        //    {return item; }
+
+        //}
+        #endregion
+
         static void Main(string[] args)
         {
             #region Q1: What is a generic class? Why use generics?
             //  class that can work with any type using a type parameter <T> 
             // becouse code relabilty and provid error , better performance , clear code 
+            #endregion
+            #region Q2: Write a generic class Container<T> with Add and Get methods.
+            //continer<int>  obj1= new continer<int> ();
+            //obj1.Add(1);
+            //Console.WriteLine(obj1.GET());
+
+            //continer<string> obg2 = new continer<string> ();
+            //obg2.Add("Hello");
+            //Console.WriteLine(obg2.GET());
+
             #endregion
 
         }
