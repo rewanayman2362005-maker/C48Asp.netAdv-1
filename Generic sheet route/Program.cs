f5ac84de0f15bfc295f02c13f49fe5d17d526fa2
rewanayman2362005 <rewanayman2362005@gmail.com>
2026-08-22 20:13:01 +0300

    Quession1

--- Generic sheet route/Program.cs
+++ Generic sheet route/Program.cs
@@ -5,7 +5,11 @@ namespace Generic_sheet_route
     {
         static void Main(string[] args)
         {
-            
+            #region Q1: What is a generic class? Why use generics?
+            //  class that can work with any type using a type parameter <T> 
+            // becouse code relabilty and provid error , better performance , clear code 
+            #endregion
+
         }
     }
 }
